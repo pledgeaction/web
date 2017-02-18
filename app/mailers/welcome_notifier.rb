6df13905c30aeefcc47358c0c16fa4afd6e1ca44
Daniel Hernandez-Stewart <danny@Daniels-MacBook-Pro.local>
2017-02-18 12:53:43 -0800

    added working welcome and advisor emails

---
+++ app/mailers/welcome_notifier.rb
@@ -0,0 +1,24 @@
+include SendGrid
+class WelcomeNotifier < ApplicationMailer
+  def send_welcome_email(user)
+    puts "send_welcome_email"
+    if user.nil?
+        puts "nil user or user declined to start conversations"
+        return
+    end
+
+    @user = user
+
+    if user.hours_pledged == 1
+      subject = "Thanks for your " + user.hours_pledged.round.to_s \
+              + " hour a week pledge towards political action"
+    else
+      subject = "Thanks for your " + user.hours_pledged.round.to_s \
+              + " hours a week pledge towards political action"
+    end
+
+    mail( :to => user[:email],
+          :subject => subject
+        )
+  end
+end
