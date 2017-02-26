b8f63985d7960b89ee6c6ca2ebdff11aba3d18ad
Ben Swartz <benswa@gmail.com>
2017-02-26 15:39:03 -0800

    add small fb integration

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ With heroku credentials, call:
 ```
 DATABASE_URL=$(heroku config:get DATABASE_URL -a pledge-action) bundle exec rails s -e hot_development
 ```
+
+For development add the following line to `/etc/hosts` (use sudo)
+```
+127.0.0.1       localhost.pledgeaction.org
+```
--- app/assets/javascripts/success.js
+++ app/assets/javascripts/success.js
@@ -2,6 +2,14 @@ $(window).load(function () {
   if ($('.pages.success').length) {
     $.ajax('/ref.json' + decodeURIComponent(window.location.search)).then(function (res) {
       $('a.referral.ui-link').attr('href', "/?ref_user=" + res.userUrl);
+      // $('.button.facebook').click(function () {
+      //   FB.ui({
+      //       method: 'share',
+      //       href: 'http://www.pledgeaction.org?ref_user=' + res.userUrl,
+      //       quote: "Take the Pledge!"
+      //   });
+      // });
     });
+
   }
 });
