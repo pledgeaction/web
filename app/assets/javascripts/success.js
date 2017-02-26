9f4768d5d73271c5ad153c353d75395b575e26fc
Ben Swartz <benswa@gmail.com>
2017-02-26 15:00:46 -0800

    remove whitespace and console.log

--- app/assets/javascripts/success.js
+++ app/assets/javascripts/success.js
@@ -1,8 +1,6 @@
-
 $(window).load(function () {
   if ($('.pages.success').length) {
     $.ajax('/ref.json' + decodeURIComponent(window.location.search)).then(function (res) {
-      console.log(res);
       $('a.referral.ui-link').attr('href', "/?ref_user=" + res.userUrl);
     });
   }
