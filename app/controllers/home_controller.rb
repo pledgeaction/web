9228b38979bb006af57a042539a940ce0c77b908
Josh Tabak <tabak.josh@gmail.com>
2017-03-04 15:33:12 -0800

    fix race condition

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,9 +5,25 @@ class HomeController < ApplicationController
 
   def success
     if params[:email]
-      ref_url = User.where(:email => params[:email]).last.url
-      redirect_to '/success?ref_user=' + ref_url
-      return
+      # look for recently created user and get their referral code
+      ref_url = User.where(:email => params[:email]).last.try(:url)
+      if ref_url
+        redirect_to '/success?ref_user=' + ref_url
+        return
+      end
+
+      try_count = params[:try].try(:to_i) || 0
+
+      # loop this endpoint until the new user exists in the DB
+      if try_count < 4
+        sleep(0.2)
+
+        redirect_to '/success?email=' params[:email] + '&try=' + (try_count + 1).to_s
+        return
+      else
+        render_404
+        return
+      end
     end
 
     @share_url = request.base_url + '/u/' + params[:ref_user]
