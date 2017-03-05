c8480e81f4ffc68ee11f795a63d0581a260bff34
Daniel Hernandez-Stewart <danny@Daniels-MacBook-Pro.local>
2017-03-04 19:55:59 -0800

    working dannybot still weak on causes

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -3,7 +3,8 @@ class SmsController < ApplicationController
    #skip_before_filter :authenticate_user!, :only => "reply"
 
   def checkin
-    #TODO setup a local dev number
+    #TODO write answer of have_organization to database.
+    #TODO remind people of people and organizations they thought they would want to work with.
     message_body = params["Body"]
     from_number = params["From"]
     boot_twilio
@@ -11,25 +12,38 @@ class SmsController < ApplicationController
     @user = User.where(phone_number: from_number).where(enable_text_checkins: true).last
 
     if message_body == "0"
-      #TODO look up their pledge amount, give them a friendly reminder of that.
-      render plain: "Happens, I recommend making a plan right now for the next couple weeks"
       @checkin.update(hours: 0)
+      if @user.has_working_group == nil or @user.has_working_group == false
+        @checkin.update(last_question: "have_working_group")
+        render plain: "Happens, I recommend making a plan right now for the next couple weeks.
+
+Have you got at least one friend committed to work with you?"
+        return
+      else
+        @checkin.update(last_question: "have_organization")
+        render plain: "Happens, I recommend making a plan right now for the next couple weeks.
+Have you found an organization to help? (or started your own)"
+        return
+      end
     elsif (1 .. 24 * 7).member?(message_body.to_i)
       render plain: "Dope! Keep up the good work."
       @checkin.update(hours: message_body.to_i)
+      return
     end
 
     case message_body.downcase
     when "unsub"
       #TODO verify all the yes's work, the no's work as intended.
-      
+
       #TODO maybe prompt them to reduce their pledge hours at this point.
       @user.update(enable_text_checkins: false)
       render plain: 'Kk. You\'ve been unsubscribed from these check ins. Text "resub" if you\'d like to resubscribe'
+
     when "resub"
       @user = User.where(phone_number: from_number).last
       @user.update(enable_text_checkins: true)
       render plain: 'And we\'re back! You\'ve been resubscribed to these check ins. Text "unsub" if you\'d like to unsubscribe'
+
     when "how"
       if @user.has_working_group == nil or @user.has_working_group == false
         @checkin.update(last_question: "have_working_group")
@@ -38,23 +52,32 @@ class SmsController < ApplicationController
         @checkin.update(last_question: "have_organization")
         render plain: "Have you found an organization to help? (or started your own)"
       end
+
     when "yes"
       case @checkin.last_question
       when "have_working_group"
         @user.update(has_working_group: true)
         @checkin.update(last_question: "have_organization")
-        render plain: "Have you found an organization to volunteer/work for? (or started your own)"
+        render plain: "Amazing. The buddy system is mission critical here.
+
+Have you found an organization to volunteer/work for? (or started your own)"
       when "get_working_group_this_week"
         render plain: "Then focus on that for sure. That'd be huge"
       when "get_working_group_this_month"
         render plain: "Then focus on that for sure. That'd be huge"
       when "have_organization"
-        render plain: "That's dope. Sounds like you are set"
+        render plain: "That's dope. Your squad is way ahead of the curve."
       when "help_pledge_directly"
-        render plain: "Amazing. Email join@pledgeaction.org and we'll get you sorted"
+        render plain: "Amazing!
+
+Email volunteer@pledgeaction.org and we'll get you sorted"
+
       when "expect_to_fulfill_next_week"
-        render plain: "That's dope. Sounds like you are set"
-    end
+        render plain: "That's dope!
+
+I'm so glad I could help you form a plan :D"
+      end
+
     when "no"
       case @checkin.last_question
       when "have_working_group"
@@ -81,6 +104,7 @@ thesixtyfive.org/home
 
 I hope that's helpful. Do you expect to be able to fulfill your pledge next week?"
         end
+
       when "help_pledge_directly"
         @checkin.update(last_question: "expect_to_fulfill_next_week")
         render plain: "We also particularly like brandnewcongress.org
@@ -90,6 +114,7 @@ We'll keep adding more recommendations as time goes on. If you're not looking to
 thesixtyfive.org/home
 
 I hope that's helpful. Do you expect to be able to fulfill your pledge next week?"
+
       when "expect_to_fulfill_next_week"
         render plain: "A tricky case. I'm sorry I couldn't be more helpful just now. Send an email to help@pledgeaction.org and we'll do our best"
       end
