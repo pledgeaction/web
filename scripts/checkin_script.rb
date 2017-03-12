1124f3284e28fae6e49aefe4ff3ddf31b168dae6
Daniel Hernandez-Stewart <danny@Daniels-MacBook-Pro.local>
2017-03-12 16:56:22 -0700

    buddy system text progress

--- scripts/checkin_script.rb
+++ scripts/checkin_script.rb
@@ -10,41 +10,48 @@ def checkin_user(user)
   Checkin.create(:phone_number => user.phone_number)
   phone_number = user.phone_number
 
-
   begin
+    if @user.has_working_group == nil or @user.has_working_group == false
+      @checkin.update(last_question: "have_working_group")
 
-    if user.hours_pledged > 1
-      half = user.hours_pledged / 2
-      double = user.hours_pledged * 2
-      text = @client.messages.create(
-        from: ENV["TWILIO_NUMBER"],
-        to: user.phone_number,
-        body: "Hey there!
+  "We strongly believe in the buddy system. Have you got at least one friend committed to work with you?"
+
+    elsif @user.has_organization == nil or @user.has_organization == false
+      @checkin.update(last_question: "have_organization")
+    "Have you found an organization to help? (or started your own)"
+
+    else
+      if user.hours_pledged > 1
+        half = user.hours_pledged / 2
+        double = user.hours_pledged * 2
+        text = @client.messages.create(
+          from: ENV["TWILIO_NUMBER"],
+          to: user.phone_number,
+          body: "Hey there!
 
 This is Danny checking in on your pledge (#{user.hours_pledged.round.to_s} hours per week).
 How many hours did you spend on political action last week?
 
 REPLY with a
 number 0, #{half.round.to_s}, #{user.hours_pledged.round.to_s}, #{double.round.to_s}, etc
-HOW to get help making a high impact plan for the week
 UNSUB to unsubscribe
 "
-      )
-    else
-      text = @client.messages.create(
-        from: ENV["TWILIO_NUMBER"],
-        to: user.phone_number,
-        body: "Hey there!
+        )
+      else
+        text = @client.messages.create(
+          from: ENV["TWILIO_NUMBER"],
+          to: user.phone_number,
+          body: "Hey there!
 
 This is Danny checking in on your pledge (#{user.hours_pledged.round.to_s} hour per week).
 How many hours did you spend on political action last week?
 
 REPLY with a
 number 0, 1, 2, 3 etc
-HOW to get help making a high impact plan for the week
 UNSUB to unsubscribe
 "
-      )
+        )
+      end
     end
   rescue Exception => e
     if e.code == 21211
@@ -57,27 +64,27 @@ end
 
 # Test user
 # checkin_user(User.find(id=504)) invalid number
-# checkin_user(User.find(id=515))
+checkin_user(User.find(id=515))
 
-User.where.not(:phone_number => nil).where.not(:enable_text_checkins => false).find_each do |user|
-  puts ""
-  puts user.name
-  puts user.phone_number
-
-# in case job failed half way through
-  # if ["+13166193650", "+18472198157", "+14846865329", "+18083521086"].include? user.phone_number
-  #   puts "skipping"
-  #   next
-  # end x
-  puts "texting"
-
-  #UNCOMMENT TO ACTUALLY RUN
-  # checkin_user(user)
-
-  @checkin = Checkin.where(phone_number: user.phone_number).last
-  if @checkin
-    puts @checkin.created_at
-    puts @checkin.hours
-    puts @checkin.last_question
-  end
-end
+# User.where.not(:phone_number => nil).where.not(:enable_text_checkins => false).find_each do |user|
+#   puts ""
+#   puts user.name
+#   puts user.phone_number
+#
+# # in case job failed half way through
+#   # if ["+13166193650", "+18472198157", "+14846865329", "+18083521086"].include? user.phone_number
+#   #   puts "skipping"
+#   #   next
+#   # end x
+#   puts "texting"
+#
+#   #UNCOMMENT TO ACTUALLY RUN
+#   # checkin_user(user)
+#
+#   @checkin = Checkin.where(phone_number: user.phone_number).last
+#   if @checkin
+#     puts @checkin.created_at
+#     puts @checkin.hours
+#     puts @checkin.last_question
+#   end
+# end
