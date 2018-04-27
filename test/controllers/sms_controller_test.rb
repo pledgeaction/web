b183b05f20ebdff4158703677a0e00741a1c3993
Darragh Buckley <darragh.buckley@gmail.com>
2018-04-27 07:36:30 -0700

    Update deprecated method name.
    This also adds a test to catch this error in the future.

--- test/controllers/sms_controller_test.rb
+++ test/controllers/sms_controller_test.rb
@@ -1,4 +1,8 @@
 require 'test_helper'
 
 class SmsControllerTest < ActionController::TestCase
+  test "should 200 if params are as expected" do
+    response = post :checkin, params: { Body: '' }
+    assert response.code == '200'
+  end
 end
